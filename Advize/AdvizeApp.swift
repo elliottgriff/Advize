ae804a5d586bf2550061c18be2f058feb6f88130
elliottcgriffin <elliott.griffin@foyer.ai>
2025-01-07 21:15:04 -0500

    Initial Commit

---
+++ Advize/AdvizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  AdvizeApp.swift
+//  Advize
+//
+//  Created by Elliott Griffin on 1/7/25.
+//
+
+import SwiftUI
+
+@main
+struct AdvizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AdvizeTests/AdvizeTests.swift
@@ -0,0 +1,17 @@
+//
+//  AdvizeTests.swift
+//  AdvizeTests
+//
+//  Created by Elliott Griffin on 1/7/25.
+//
+
+import Testing
+@testable import Advize
+
+struct AdvizeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
